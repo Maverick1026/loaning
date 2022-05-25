ae3eea70a20499cf816c925d69d25042f20f28ac
Maverick1026 <105482425+Maverick1026@users.noreply.github.com>
2022-05-26 03:36:43 +0800

    Add files via upload

---
+++ main/main.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>SSS Loan Management</title>
+    </head>
+    <body>
+        <div id="main">
+            <h1>Welcome to SSS Loan Management</h1>
+            <h3>Who are you?</h3>
+            <a href="/Loaning/admin/login.php"><div class="link">Admin</div></a>
+            <a href="/Loaning/user/login.php"><div class="link">User</div></a>
+        </div>
+</body>
+</html>
